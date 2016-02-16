0548ac6664ef8387ad2d599786462a848e7d505d
yistand <yistand@gmail.com>
2016-02-16 14:56:44 -0500

    add simple code for Ntrk distribution in ppMB

---
+++ TimeDep/Ntrk.cxx
@@ -0,0 +1,244 @@
+//====================================================================================================
+//
+//	2016.02.16	Li YI
+//	simply code to plot mulitplicity distribution with the same cut as undelrying vs jet study
+//
+//====================================================================================================
+
+#include "AjParameters.hh"
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TProfile.h>
+#include <TFile.h>
+
+#include <TLorentzVector.h>
+#include <TClonesArray.h>
+#include <TString.h>
+#include <TChain.h>
+#include <TBranch.h>
+#include <TMath.h>
+#include <TRandom.h>
+#include <TSystem.h>
+
+#include "TClonesArray.h"
+
+#include <utility>	// std::pair, std::make_pair
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <set>
+#include <cmath>
+#include <exception>
+#include <cstdlib>      // std::rand, std::srand
+#include <algorithm>    // std::random_shuffle
+
+#include "TStarJetPicoReader.h"
+#include "TStarJetPicoEvent.h"
+#include "TStarJetPicoEventHeader.h"
+#include "TStarJetPicoEventCuts.h"
+
+#include "TStarJetPicoPrimaryTrack.h"
+#include "TStarJetPicoTrackCuts.h"
+#include "TStarJetPicoTowerCuts.h"
+
+#include "TStarJetPicoTriggerInfo.h"
+
+
+using namespace std;
+
+
+
+bool readinbadrunlist(std::set<int> & badrun, TString csvfile="pp200Y12_badrun.list") {
+	
+	// open infile
+	std::string line;
+	std::ifstream inFile (csvfile );
+	
+	std::cout<<"Loading bad run id from "<< csvfile.Data()<<std::endl;;
+	        
+	if ( !inFile.good() ) {
+	  std::cout<<"Can't open "<<csvfile.Data()<<std::endl;
+	  return false;
+	}
+	
+	while (std::getline (inFile, line) ){
+	  if ( line.size()==0 ) continue; // skip empty lines
+	  if ( line[0] == '#' ) continue; // skip comments
+	
+	  std::istringstream ss( line );
+	  while( ss ){
+	    std::string entry;
+	    std::getline( ss, entry, ',' );
+	    int ientry = atoi(entry.c_str());
+	    if (ientry) {
+	      badrun.insert( ientry );
+	      std::cout<<"Added bad runid "<<ientry<<std::endl;
+	    }
+	  }
+	}
+	
+	return true;
+}
+
+// Helper to deal with repetitive stuff
+TStarJetPicoReader SetupReader ( TChain* chain, TString TriggerString, const double RefMultCut ){
+	TStarJetPicoDefinitions::SetDebugLevel(10); // 10 for more output, 0 for less output
+
+	TStarJetPicoReader reader;
+	reader.SetInputChain (chain);
+
+	// Event and track selection
+	// -------------------------
+	TStarJetPicoEventCuts* evCuts = reader.GetEventCuts();
+	evCuts->SetTriggerSelection( TriggerString ); //All, MB, HT, pp, ppHT, ppJP
+	// Additional cuts 
+	evCuts->SetVertexZCut (AjParameters::VzCut);
+	evCuts->SetRefMultCut ( RefMultCut );
+	evCuts->SetVertexZDiffCut( AjParameters::VzDiffCut );
+
+	evCuts->SetMaxEventPtCut ( AjParameters::MaxEventPtCut );
+	evCuts->SetMaxEventEtCut ( AjParameters::MaxEventEtCut );
+
+	evCuts->SetPVRankingCut ( 0 );		// Vertex ranking > 0. Use SetPVRankingCutOff() to turn off vertex ranking cut.  default is OFF
+
+	std::cout << "Exclude event with track > " << evCuts->GetMaxEventPtCut() << std::endl;
+	std::cout << "Exclude event with tower > " << evCuts->GetMaxEventEtCut() << std::endl;
+
+	// Tracks cuts
+	TStarJetPicoTrackCuts* trackCuts = reader.GetTrackCuts();
+	trackCuts->SetDCACut( AjParameters::DcaCut );
+	trackCuts->SetMinNFitPointsCut( AjParameters::NMinFit );
+	trackCuts->SetFitOverMaxPointsCut( AjParameters::FitOverMaxPointsCut );
+	trackCuts->SetMaxPtCut ( AjParameters::MaxTrackPt );
+
+	std::cout << "Using these track cuts:" << std::endl;
+	std::cout << " dca : " << trackCuts->GetDCACut(  ) << std::endl;
+	std::cout << " nfit : " <<   trackCuts->GetMinNFitPointsCut( ) << std::endl;
+	std::cout << " nfitratio : " <<   trackCuts->GetFitOverMaxPointsCut( ) << std::endl;
+	std::cout << " maxpt : " << trackCuts->GetMaxPtCut (  ) << std::endl;
+
+	// Towers
+	TStarJetPicoTowerCuts* towerCuts = reader.GetTowerCuts();
+	towerCuts->SetMaxEtCut(AjParameters::MaxEtCut);
+	char *path = getenv("PPJET");
+	towerCuts->AddBadTowers(Form("%s/include/pp200Y12_badtower.list",path));		// #LY CHECK where is the bad tower list
+
+	// Tower energy correction (subtract associated charged particle deposit energy). By default, it is MIP correction (comment out the following 3 lines)
+	reader.SetApplyFractionHadronicCorrection(kTRUE);
+	reader.SetFractionHadronicCorrection(0.9999);
+	reader.SetRejectTowerElectrons( kFALSE );
+
+
+	std::cout << "Using these tower cuts:" << std::endl;
+	std::cout << "  GetMaxEtCut = " << towerCuts->GetMaxEtCut() << std::endl;
+
+	// V0s: Turn off
+	reader.SetProcessV0s(false);
+
+	return reader;
+
+}
+
+
+int main( int argc, const char** argv ) {
+
+	// Set up some convenient default
+	// ------------------------------
+	const char *defaults[] = {"Ntrk","Ntrk.root","ppMB","/home/fas/caines/ly247/Scratch/run12ppQA/pp200Y12PicoMB_sum*.root"};
+
+	if ( argc==1 ) {
+		argv=defaults;
+		argc=sizeof (defaults ) / sizeof (defaults[0] );
+	}
+
+	// Throw arguments in a vector
+	// ---------------------------
+	vector<string> arguments(argv + 1, argv + argc);
+
+	// Load and set up tree
+	// --------------------
+	TString ChainName  = "JetTree";
+	TString OutFileName = arguments.at(0);
+
+
+	cout<<"TriggerName: "<<arguments.at(1)<<endl;
+	TString TriggerName = arguments.at(1);
+
+
+	cout<<"Chain data: "<<arguments.at(2).data()<<" for "<<ChainName<<endl;
+	TChain* chain = new TChain( ChainName );
+	chain->Add( arguments.at(2).data() );
+
+
+	cout<<"SetupReader for pico"<<endl;
+	double RefMultCut = 0;
+	TStarJetPicoReader reader = SetupReader( chain, TriggerName, RefMultCut );			// #ly note: Events & Tracks & Towers cuts are set here
+	//reader.SetTrackPileUpCut(kTRUE);		// #ly	tpc track matching to bemc or tof
+	reader.SetTrackPileUpCut(2);		// #ly	1: tpc track matching to bemc or tof. 	2: tof match only.    0: no requirement for fast detector matching
+	//reader.SetTrackPileUpCut(0);		// #ly	1: tpc track matching to bemc or tof. 	2: tof match only.    0: no requirement for fast detector matching	// test
+	TStarJetPicoDefinitions::SetDebugLevel(0);
+
+	TStarJetVectorContainer<TStarJetVector>* container;		// for underlying event loop
+	TStarJetVector* sv; // TLorentzVector* would be sufficient. 
+
+	Long64_t nEvents=-1; // -1 for all
+	//nEvents=10000;	// test
+	cout<<"init..."<<endl;
+	reader.Init(nEvents);
+	int count = 0;
+
+	std::set<int>badrun;
+	badrun.clear();
+
+	char *ppjetpath = getenv("PPJET");
+	readinbadrunlist(badrun,Form("%s/pp200Y12_badrun.list",ppjetpath));        
+
+
+	TH1D *hNtrk = new TH1D("hNtrk","Charged multiplicity",100,0,100);
+
+	try{
+		while ( reader.NextEvent() ) {
+			reader.PrintStatus(10);
+			if(count%10000==0) cout<<"event "<<count<<endl;
+			count++;
+
+			// event info
+			// ----------
+			//cout<<"load event header"<<endl;
+			TStarJetPicoEventHeader* header = reader.GetEvent()->GetHeader();
+
+			// eventid = header->GetEventId();
+			int runid   = header->GetRunId();
+			if(badrun.count(runid)>0) continue;			// in bad run list
+
+
+			// Load event particles
+			// ----------
+			container = reader.GetOutputContainer();
+
+			int count = 0; 
+			for (int ip = 0; ip<container->GetEntries() ; ++ip ){
+				sv = container->Get(ip);  // Note that TStarJetVector contains more info, such as charge;
+
+				if(fabs(sv->perp())<0.2) continue;		// #ly CHECK!!!!!!!! minimum pT or Et. --> NOT in use anymore, moved cuts to UnderlyingAna class min_const_pt for all particles (tracks and towers)
+				if(sv->GetCharge()==0 ) continue;
+
+				count++;
+			}    
+			hNtrk->Fill(count);
+		} // while NextEvent
+	} catch ( exception& e) {
+		cerr << "Caught " << e.what() << endl;
+		return -1;
+	}
+	cout << "##################################################################" << endl;
+
+	TFile *fout = new TFile("Ntrk_ppMB.root","RECREATE");
+	hNtrk->Write();
+	fout->Close();
+	
+	cout << " DONE " << endl;
+
+}
